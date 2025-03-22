f6d2315105671e6493a3295937d12bd9c0b551d7
Md. Salauddin <salauddinahmed555@gmail.com>
2025-03-23 02:02:59 +0600

    Conncected to the MongoDb

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,35 @@
-import { Server } from 'http';
-import app from './app';
+import { Server } from "http";
+import app from "./app";
+import dotenv from "dotenv";
+
+import mongoose from "mongoose";
+dotenv.config(); // Load environment variables
 
 const PORT = 3000;
 let server: Server;
 
+const uri: any = process.env.DATABASE_URL;
+console.log(uri);
+async function connectDB() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri);
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+
+
+
 async function bootstrap() {
+  await connectDB(); // Ensure the database connects before starting the server
+
   server = app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
   });
 }
 
